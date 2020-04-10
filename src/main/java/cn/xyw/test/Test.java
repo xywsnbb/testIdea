5651011c429cd79286ec91c0e73f403ed39942b4
admin <a468355955@163.com>
2020-04-10 11:51:57 +0800

    test

---
+++ src/main/java/cn/xyw/test/Test.java
@@ -0,0 +1,12 @@
+package cn.xyw.test;
+
+/**
+ * @Author xyw
+ * @Date 2020/4/10 10:28
+ * @Version 1.0
+ **/
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
